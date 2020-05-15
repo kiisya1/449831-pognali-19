b012390ddfac74aca290e482222eb36685190801
Anna Pronina <kiisya1@yandex.ru>
2020-05-15 11:34:45 +0300

    Правит грамматику в названии классов

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task("images", function () {
 });
 
 gulp.task("webp", function () {
-  return gulp.src(["source/img/**/*.{png,jpg}", "!source/img/{rogovaya-back-,smolov-back-,rates,globe-back-,flags-back,devider,about-,traveller-illustration-}*.{png,jpg}"])
+  return gulp.src(["source/img/**/*.{png,jpg}", "!source/img/{rogovaya-back-,smolov-back-,rates,globe-back-,flags-back,divider,about-,traveller-illustration-}*.{png,jpg}"])
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img"));
 });
